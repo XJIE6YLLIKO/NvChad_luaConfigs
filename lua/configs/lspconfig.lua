ae2b1ba3f97cbad1f9c2f85ce753f120680029a3
XJIE6 <fazz25@duck.com>
2024-11-20 22:48:39 +0300

    Delete jq_lsp, cus' Im still too lazy

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "nixd", "clangd", "basedpyright", "jsonls", "ast_grep", "ts_ls", "jq-lsp" }
+local servers = { "html", "cssls", "nixd", "clangd", "basedpyright", "jsonls", "ast_grep", "ts_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
